046288c6273b81ea8a596b2d3c7839d35866cce0
smkrizan <smkrizan@gmail.com>
2020-09-30 16:01:51 -0500

    final edits incorporated

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,12 +53,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     // array in which to store the task data
@@ -229,7 +235,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -263,9 +269,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
